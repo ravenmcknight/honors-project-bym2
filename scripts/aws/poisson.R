a2597dd9c7f66d89f296f64cffea01ca4fdc5cba
Raven McKnight <raven.mcknight@metrotransit.org>
2020-01-21 11:19:55 -0600

    push new stan to run

--- scripts/aws/poisson.R
+++ scripts/aws/poisson.R
@@ -23,14 +23,25 @@ N <- nrow(mod_dat)
 K <- ncol(xdat)
 x <- as.matrix(ncol = K, xdat)
 
-standat1 <- list(y = as.integer(y/365), E = E, x = x, K = K, N = N)
+standat1 <- list(y = y, E = E, x = x, K = K, N = N)
+
+horseshoe_dat <- list(y = y, E = E, x = x, K = K, N = N, 
+                      scale_icept = 10, scale_global = 0.2, 
+                      nu_global = 1, nu_local = 1,
+                      slab_scale = 2, slab_df = 4)
 
 ## basic poisson --------------------------------
 
 poisson <- "~/Documents/honors/honors-project/stan/poisson.stan"
-poisson_fit <- stan(poisson, data = standat1, warmup = 1000, iter = 2000, verbose = T)
+poisson_fit <- stan(poisson, data = standat1, warmup = 100, iter = 200, verbose = T)
 
 saveRDS(poisson_fit, '~/Documents/honors/honors-project/final-fits/poisson_fit.RDS')
+
+## add horseshoe priors -------------------------
+
+poisson_hrs <- "~/Documents/honors/honors-project/stan/poisson_horseshoe.stan"
+poisson_hrs_fit <- stan(poisson_hrs, data = horseshoe_dat, warmup = 100, iter = 200, verbose = T)
+
 
 ## add overdispersion parameter -----------------
 
@@ -43,10 +54,7 @@ poisson_theta_fit <- stan(poisson_theta, data = standat1, iter = 2000, verbose =
 
 poisson_horseshoe <- "~/Documents/honors/honors-project/stan/poisson_theta_horseshoe.stan"
 
-horseshoe_dat <- list(y = y, E = E, x = x, K = K, N = N, 
-                  scale_icept = 10, scale_global = 0.2, 
-                  nu_global = 1, nu_local = 1,
-                  slab_scale = 2, slab_df = 4)
+
 
 poisson_horseshoe_fit <- stan(poisson_horseshoe, data = horseshoe_dat, iter = 200, verbose = T, 
                               control = list(adapt_delta = 0.99))
